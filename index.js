f84b93abbc890ff02bc7b415fa809815d759fede
Paulo Júnior <pauloharjunior@gmail.com>
2025-01-14 17:03:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+ import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
+import verificarListavazia from "./scripts/verificarListaVazia.js";
+
+ const listaDeCompras = document.getElementById('lista-de-compras');
+const botaoAdicionar = document.getElementById('adicionar-item')
+
+botaoAdicionar.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    const itemDaLista = criarItemDaLista();
+    listaDeCompras.appendChild(itemDaLista);
+    verificarListavazia(listaDeCompras);
+})
+
+
+
+verificarListavazia(listaDeCompras);
